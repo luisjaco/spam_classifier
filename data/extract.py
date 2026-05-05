fe4aa17a96cfae31a83614f18ea5fd90edb1e837
Luis Jaco <lmario329@gmail.com>
2026-05-05 16:16:50 -0400

    Alter project structure.

--- data/extract.py
+++ data/extract.py
@@ -4,16 +4,16 @@ This file will unzip all of the .tar.bz2 files found within the ./data dictionar
 import tarfile
 
 print('--------------------extracting easy_ham_2--------------------')
-with tarfile.open('./data/20030228_easy_ham_2.tar.bz2', 'r:bz2') as f:
+with tarfile.open('./20030228_easy_ham_2.tar.bz2', 'r:bz2') as f:
     f.list(verbose=True)
-    f.extractall('./data/')
+    f.extractall()
 
 print('--------------------extracting hard_ham--------------------')
-with tarfile.open('./data/20030228_hard_ham.tar.bz2', 'r:bz2') as f:
+with tarfile.open('./20030228_hard_ham.tar.bz2', 'r:bz2') as f:
     f.list(verbose=True)
-    f.extractall('./data/')
+    f.extractall()
 
 print('--------------------extracting spam_2--------------------')
-with tarfile.open('./data/20050311_spam_2.tar.bz2', 'r:bz2') as f:
+with tarfile.open('./20050311_spam_2.tar.bz2', 'r:bz2') as f:
     f.list(verbose=True)
-    f.extractall('./data/')
+    f.extractall()
